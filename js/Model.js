9cdbb115c35cdbea67b335453d7777f36982320b
Kristina Pchelintseva <kristinapchelincewa@gmail.com>
2019-06-20 01:39:54 +0300

    Добавлены имена игроков

--- js/Model.js
+++ js/Model.js
@@ -9,6 +9,8 @@ class Model {
         };
         this._winCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
         this.listeners = new Set();
+        this.firstPlayer = 'PlayerX';
+        this.secondPlayer = 'PlayerO';
     }
 
     subscribe(listener) {
