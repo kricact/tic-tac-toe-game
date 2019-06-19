cb4d0ecd920aac855b517ed0ec75b353c9b39d8f
Kristina Pchelintseva <kristinapchelincewa@gmail.com>
2019-06-20 01:54:25 +0300

    Реализовано взаимодействие игрока с полем

--- js/View.js
+++ js/View.js
@@ -1,12 +1,13 @@
 class View {
     constructor(model) {
-        this.model = model;
+        this._firstPlayer = model.firstPlayer;
+        this._secondPlayer = model.secondPlayer;
         this._field = document.querySelectorAll('.field__item');
+        this.currentPlayer = this._firstPlayer;
     }
 
     showEndNotification() {
         alert('Кто-то win');
-        console.log(this._field);
     }
 
 
@@ -15,12 +16,23 @@ class View {
     }
 
     bind() {
-        this._field.forEach((currentValue, i, array) => {
+        this._field.forEach((currentValue, i) => {
             currentValue.addEventListener('click', ()=> {
-                this.onPlayerMove({'id':i, 'player':'playerX'});
+                this.updateField(currentValue, this.currentPlayer);
+                this.changePlayer();
+                this.onPlayerMove({'id':i, 'player':this.currentPlayer});
             });
         });
 
+    }
+
+    updateField (el, player) {
+        el.classList.add(player);
+        el.disabled = true;
+    }
+
+    changePlayer () {
+        this.currentPlayer = (this.currentPlayer === this._firstPlayer) ? this._secondPlayer: this._firstPlayer;
     }
 
     onPlayerMove(step) {
